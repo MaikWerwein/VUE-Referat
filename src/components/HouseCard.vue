d1447d3f9155dbe149fb3e1d10c6f140a5c26dd3
nikolas.medick <gh@makiroll.dev>
2024-06-06 13:28:58 +0200

    basic house card

--- src/components/HouseCard.vue
+++ src/components/HouseCard.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="lg:col-2 md:col-3 col-6 card-outer">
-    <div data-status="ok" class="card-content">{{ house.adresse }}</div>
+    <div data-status="ok" class="card-content">
+      <div class="header flex vertical-align-middle justify-space-between">
+        <IconHouse />
+        <span class="title-text pl-2">
+          {{ house.adresse }}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +19,16 @@
 .card-content {
   padding: 6px;
   border-radius: 5px;
+  min-height: 100px;
+}
+
+.title-text {
+  font-size: 125%;
+}
+
+.header {
+  /* ???????? */
+  border-bottom: solid 1px var(--color-border);
 }
 
 .card-content[data-status='ok'] {
@@ -21,6 +38,7 @@
 
 <script setup>
 import { defineProps } from 'vue'
+import IconHouse from './icons/IconHouse.vue'
 
 defineProps({ house: null })
 </script>
